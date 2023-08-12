bac53fb11027810dbb5e4ffb1b7ce6e299628e01
Caferino <caferinograal@hotmail.com>
2023-08-11 18:54:49 -0600

    Dashboard Content Structure

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link signin.css
+//= link navbar.css
+//= link dashboard.css
+//= link dashboardContent.css
