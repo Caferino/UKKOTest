75cb544a2ed1567e1a871a34f875b804478cf451
Caferino <caferinograal@hotmail.com>
2023-08-16 14:47:21 -0600

    Check session for every action or URL taken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    before_action :check_session
+
+    private
+      
+    def check_session
+        return if session[:user_id].present?
+
+        redirect_to users_index_path, alert: "Please, log in."
+    end
+
 end
