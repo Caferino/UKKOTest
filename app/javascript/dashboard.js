a948853319f8a1db638c9e7caaa61d54dac2af25
Caferino <caferinograal@hotmail.com>
2023-08-17 22:16:25 -0600

    Partial Books Tab Functionality

--- app/javascript/dashboard.js
+++ app/javascript/dashboard.js
@@ -1,16 +1,40 @@
 function loadContent(contentType) {
     $.ajax({
-      url: '/load_content_vanilla',
+      url: '/load_content',
       method: 'GET',
       data: { content_type: contentType },
       success: function(response) {
-        $('.container-for-content').html(response);
+        // $('.container-for-content').html(response);
+        console.log("Successful Load.")
       }
     });
   }
 
-  function toggleContent() {
-    const storesContent = document.getElementById('container-for-content');
-    storesContent.classList.toggle('hiddenContent');
+  function load_content(contentType) {
+    const storesContent = document.getElementById('container-for-stores');
+    const booksContent = document.getElementById('container-for-books');
+
+    if (contentType == 'stores') {
+      // The 'hiddenContent' class is present in books, meaning it's currently hidden
+      if(booksContent.classList.contains('hiddenContent')){
+        storesContent.classList.toggle('hiddenContent');
+      }
+      else {
+        booksContent.classList.toggle('hiddenContent');
+        storesContent.classList.toggle('hiddenContent');
+      }
+     
+    }
+    if (contentType == 'books') {
+      // The 'hiddenContent' class is present in books, meaning it's currently hidden
+      if(storesContent.classList.contains('hiddenContent')){
+        booksContent.classList.toggle('hiddenContent');
+      }
+      else {
+        storesContent.classList.toggle('hiddenContent');
+        booksContent.classList.toggle('hiddenContent');
+      }
+    }
+
   }
 
