7c5a00c7b58a7282fe99194ce3674f8359cb028e
Caferino <caferinograal@hotmail.com>
2023-08-20 22:10:08 -0600

    Basic Stock Management Functionality Done

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,11 +5,11 @@ class StocksController < ApplicationController
         store = Store.find(params[:store_id])
         quantity = params[:quantity].to_i
 
+        # Find or create the stock entry and update the quantity
         stock = Stock.find_or_initialize_by(book: book, store: store)
+        stock.quantity ||= 0  # Initialize to 0 if stock.quantity is nil
         stock.quantity += quantity
         stock.save
-
-        redirect_to stocks_path
     end
     
 end
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,10 +4,14 @@ class StoresController < ApplicationController
   # GET /stores or /stores.json
   def index
     @stores = Store.all
+    # Certain store with at least 1 or more books
+    @stocks = Stock.joins(:store).where('stocks.quantity > ?', 5)
   end
 
   # GET /stores/1 or /stores/1.json
   def show
+    @store = Store.find(params[:id])
+    @stocks = @store.stocks
   end
 
   # GET /stores/new
